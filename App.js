0a7fadc4d74c6e94a19bf637921e107c2a26a05b
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-05-21 06:09:23 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LoginSignup from './Components/Login-signup/Login-signup';
+
+function App() {
+  return (
+    <div>
+     <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
